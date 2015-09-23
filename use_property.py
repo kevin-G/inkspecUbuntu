6cfae7ae8b5e66fc5d72693db4cb0662287c0c8a
kevinUbuntu <guozirun@gmail.com>
2015-09-23 16:28:53 -0400

    how to use property

---
+++ use_property.py
@@ -0,0 +1,23 @@
+#use_property.py
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Student(object):
+	"""docstring for Student"""
+
+	@property
+	def score(self):
+	    return self._score
+	
+	@score.setter
+	def score(self, value):
+		if not isinstance(value, int):
+			raise ValueError('Score must be an integer!')
+		if value < 0 or value > 100:
+			raise ValueError('score must between 0 ~ 100!')
+		self._score = value
+
+s = Student()
+s.score = 60
+print('the score is :', s.score)
+s.score = 9999
