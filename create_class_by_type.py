7887a02659c52d99294441691d0f1b825a767e79
KG <wangjin@wangmatoiMac.local>
2015-09-23 21:20:30 -0400

    update

--- create_class_by_type.py
+++ create_class_by_type.py
@@ -1,3 +1,14 @@
 #!usr/bin/env python3
 # -*- coding: utf-8 
 
+def fn(self, name='World'):
+	print('Hello %s!' % name)
+
+Hello = type('Hello', (object,), dict(hello = fn))
+
+h = Hello()
+print('call h.hello():')
+
+h.hello()
+print('type(Hello) =', type(Hello))
+print('type(h) =', type(h))
